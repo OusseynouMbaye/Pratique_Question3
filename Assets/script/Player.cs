b16ec2c518ebf176335f1a013bc7e2f321e43ff6
OusseynouMbaye <60521947+OusseynouMbaye@users.noreply.github.com>
2020-10-05 12:31:05 -0400

    PlayerMovement

--- Assets/script/Player.cs
+++ Assets/script/Player.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
+    Rigidbody2D my_rigidbody;
+    float playerSpeed = 400.0f;
+    private GameObject player;
     void Start()
     {
+        my_rigidbody = gameObject.GetComponent<Rigidbody2D>();
         
     }
 
@@ -14,5 +18,21 @@ public class Player : MonoBehaviour
     void Update()
     {
         
+        float inputVertical = Input.GetAxis("Vertical");
+       // float inputFire  = Input.GetAxis("Fire");
+
+        if (inputVertical !=0) {
+            my_rigidbody.MovePosition(new Vector2(my_rigidbody.transform.position.x, my_rigidbody.transform.position.y + inputVertical));
+        
+        }
+
+        //if(inputFire != 0)
+        //{
+        //    ProcessFire();
+        //}
+    }
+    void ProcessFire()
+    {
+
     }
 }
